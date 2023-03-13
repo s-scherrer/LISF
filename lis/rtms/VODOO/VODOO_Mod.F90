8288f2e5cb92cf382541e3a792fd26dd52fbb983
Samuel Scherrer <samuel.scherrer@posteo.de>
2023-03-13 21:37:09 +0100

    updated VODOO

--- lis/rtms/VODOO/VODOO_Mod.F90
+++ lis/rtms/VODOO/VODOO_Mod.F90
@@ -21,22 +21,9 @@ module VODOO_Mod
     !
     !    VODOO parameter file:
     !        Path to the parameter file
+    !    VODOO model type:
+    !        Type of the model to use, either "window" or "anom"
     !
-    !    The model calculates VOD as linear function LAI and soil moisture;
-    !
-    !        VOD = a(LAI, RZSM) * SM' + b(LAI, RZSM)
-    !
-    !    with
-    !
-    !        SM' = relSM1 - max(0, min(1, relRZSM))
-    !        relSM1 = (SM1 - WP) / (FC - WP)
-    !        relRZSM = (RZSM - WP) / (FC - WP)
-    !        a(LAI, RZSM) = a0 * LAI + a1 * RZSM + a2 * LAI * RZSM + a3
-    !        b(LAI, RZSM) = b0 * LAI + b1 * RZSM + b2 * LAI * RZSM + b3
-    !
-    !    To use it, you need to provide a netCDF file with a single variable
-    !    "coefs", with 2 dimensions, (ngrid, ncoef). The coef dimension should
-    !    contain the coefficients in this order: a0, ..., a3, b0, ..., b3.
     !
     ! !HISTORY:
     ! 02 Mar 2022: Samuel Scherrer; initial contribution based on WCMRTM
@@ -82,13 +69,27 @@ module VODOO_Mod
         procedure, pass(self) :: run => VODOO_run_default
     end type vodoo_type_dec
 
-    type, extends(vodoo_type_dec) :: vodoo_linear_model
-        integer, parameter :: ncoef = 8
-        real, allocatable :: coefs(:, :)
+    type, extends(vodoo_type_dec) :: vodoo_window_model
+        integer, parameter :: ncoef = 2
+        ! coefs has dimensions (ngrid, doy, predictor)
+        real, allocatable :: coefs(:, :, :)
+        real, allocatable :: intercept(:, :)
         contains
-            procedure, pass(self) :: initialize => VODOO_initialize_linear_model
-            procedure, pass(self) :: run => VODOO_run_linear_model
-    end type vodoo_linear_model
+            procedure, pass(self) :: initialize => VODOO_initialize_window_model
+            procedure, pass(self) :: run => VODOO_run_window_model
+    end type vodoo_window_model
+
+    type, extends(vodoo_type_dec) :: vodoo_anom_model
+        integer, parameter :: ncoef = 5
+        real, allocatable :: vod_clim(:, :)
+        real, allocatable :: lai_clim(:, :)
+        real, allocatable :: rzsm_clim(:, :)
+        real, allocatable :: coefs(:, :)
+        real, allocatable :: intercept(:)
+        contains
+            procedure, pass(self) :: initialize => VODOO_initialize_anom_model
+            procedure, pass(self) :: run => VODOO_run_anom_model
+    end type vodoo_anom_model
 
     class(vodoo_type_dec), allocatable :: vodoo_struc(:)
 
@@ -118,11 +119,22 @@ contains
         integer :: rc, ios
         integer :: n, nid, ngrid, ngridId
         character*100 :: modeltype(LIS_rc%nnest)
-        character*10 :: predictors(LIS_rc%nnest)
         character*256 :: parameter_fname(LIS_rc%nnest)
 
         write(LIS_logunit,*) "[INFO] Starting VODOO setup"
 
+        call ESMF_ConfigFindLabel(LIS_config, "VODOO model type:",rc = rc)
+        do n=1, LIS_rc%nnest
+            call ESMF_ConfigGetAttribute(LIS_config, modeltype(n), rc=rc)
+            call LIS_verify(rc, "VODOO model type: not defined")
+        enddo
+        if (modeltype(1) .eq. "window") then
+            allocate(vodoo_window_model :: vodoo_struc(LIS_rc%nnest))
+        else
+            write(LIS_logunit, *)&
+                 "[ERR] VODOO model type must be 'window' or 'anom'"
+            call LIS_endrun
+        endif
 
         ! read config from file
         call ESMF_ConfigFindLabel(LIS_config, "VODOO parameter file:",rc = rc)
@@ -131,23 +143,13 @@ contains
             call LIS_verify(rc, "VODOO parameter file: not defined")
         enddo
 
-        ! remainder from when there where multiple models available, leaving it
-        ! here to make it easier to extend it again
-        if (.true.) then
-            allocate(vodoo_linear_model :: vodoo_struc(LIS_rc%nnest))
-        else
-            write(LIS_logunit, *)&
-                 "[ERR] VODOO model type must be 'linear' or 'SVR'"
-            call LIS_endrun
-        endif
 
         do n=1,LIS_rc%nnest
             vodoo_struc(n)%parameter_fname = parameter_fname(n)
             allocate(vodoo_struc(n)%VOD(LIS_rc%npatch(n,LIS_rc%lsm_index)))
 
             call add_sfc_fields(n,LIS_sfcState(n), "Leaf Area Index")
-            call add_sfc_fields(n,LIS_sfcState(n), "Relative Soil Moisture Layer 1")
-            call add_sfc_fields(n,LIS_sfcState(n), "Relative Root Zone Soil Moisture")
+            call add_sfc_fields(n,LIS_sfcState(n), "Root Zone Soil Moisture")
             call add_sfc_fields(n,LIS_forwardState(n),"VODOO_VOD")
         enddo
 
@@ -163,15 +165,16 @@ contains
     subroutine VODOO_initialize_default(self, n)
         class(vodoo_type_dec), intent(inout) :: self
         integer, intent(in) :: n
-        write(LIS_logunit,*) "[ERR] VODOO should use linear or svr model"
+        write(LIS_logunit,*) "[ERR] VODOO should use window or anom model"
         call LIS_endrun
     end subroutine VODOO_initialize_default
 
-    subroutine VODOO_initialize_linear_model(self, n)
+
+    subroutine VODOO_initialize_window_model(self, n)
 #if(defined USE_NETCDF3 || defined USE_NETCDF4)
         use netcdf
 #endif
-        class(vodoo_linear_model), intent(inout) :: self
+        class(vodoo_window_model), intent(inout) :: self
         integer, intent(in) :: n
 
         integer :: ios, nid, npatch
@@ -204,12 +207,72 @@ contains
         ! now that we have ntimes, we can allocate the coefficient arrays
         ! for the nest
         npatch = LIS_rc%npatch(n, LIS_rc%lsm_index)
-        allocate(self%coefs(self%ncoef, npatch))
+        allocate(self%coefs(self%ncoef, 366, npatch))
+        allocate(self%intercept(366, npatch))
 
+        call read_3d_coef_from_file(n, nid, self%ncoef, 366, ngrid, "coefs", &
+            self%coefs, ngrid_first=.false.)
+        call read_2d_coef_from_file(n, nid, 366, ngrid, "intercept", &
+            self%intercept, ngrid_first=.false.)
+#endif
+    end subroutine VODOO_initialize_window_model
+
+
+    subroutine VODOO_initialize_anom_model(self, n)
+#if(defined USE_NETCDF3 || defined USE_NETCDF4)
+        use netcdf
+#endif
+        class(vodoo_anom_model), intent(inout) :: self
+        integer, intent(in) :: n
+
+        integer :: ios, nid, npatch
+        integer :: ngridId, ngrid
+
+#if !(defined USE_NETCDF3 || defined USE_NETCDF4)
+        write(LIS_logunit,*) "[ERR] VODOO requires NETCDF"
+        call LIS_endrun
+#else
+        ! try opening the parameter file
+        write(LIS_logunit,*) '[INFO] Reading ',&
+            trim(self%parameter_fname)
+        ios = nf90_open(path=trim(self%parameter_fname),&
+            mode=NF90_NOWRITE,ncid=nid)
+        call LIS_verify(ios,'Error opening file '&
+            //trim(vodoo_struc(n)%parameter_fname))
+
+        ! check if ngrid is as expected
+        ios = nf90_inq_dimid(nid, "ngrid", ngridId)
+        call LIS_verify(ios, "Error nf90_inq_varid: ngrid")
+        ios = nf90_inquire_dimension(nid, ngridId, len=ngrid)
+        call LIS_verify(ios, "Error nf90_inquire_dimension: ngrid")
+        if (ngrid /= LIS_rc%glbngrid_red(n)) then
+            write(LIS_logunit, *) "[ERR] ngrid in "//trim(self%parameter_fname)&
+                 //" not consistent with expected ngrid: ", ngrid,&
+                 " instead of ",LIS_rc%glbngrid_red(n)
+            call LIS_endrun
+        endif
+
+        ! now that we have ntimes, we can allocate the coefficient arrays
+        ! for the nest
+        npatch = LIS_rc%npatch(n, LIS_rc%lsm_index)
+        allocate(self%vod_clim(366, npatch))
+        allocate(self%lai_clim(366, npatch))
+        allocate(self%rzsm_clim(366, npatch))
+        allocate(self%coefs(5, npatch))
+        allocate(self%intercept(npatch))
+
+        call read_2d_coef_from_file(n, nid, 366, ngrid, "vod_climatology", &
+            self%vod_clim, ngrid_first=.false.)
+        call read_2d_coef_from_file(n, nid, 366, ngrid, "lai_climatology", &
+            self%lai_clim, ngrid_first=.false.)
+        call read_2d_coef_from_file(n, nid, 366, ngrid, "rzsm_climatology", &
+            self%rzsm_clim, ngrid_first=.false.)
         call read_2d_coef_from_file(n, nid, self%ncoef, ngrid, "coefs", &
             self%coefs, ngrid_first=.false.)
+        call read_1d_coef_from_file(n, nid, ngrid, "intercept", self%intercept)
 #endif
-    end subroutine VODOO_initialize_linear_model
+    end subroutine VODOO_initialize_anom_model
+
 
     subroutine read_1d_coef_from_file(n, nid, ngrid, varname, coef)
 #if(defined USE_NETCDF3 || defined USE_NETCDF4)
@@ -244,6 +307,16 @@ contains
     end subroutine read_1d_coef_from_file
 
     subroutine read_2d_coef_from_file(n, nid, n1, n2, varname, coef, ngrid_first)
+        ! Parameters
+        ! ----------
+        ! n: nest index
+        ! nid: netcdf file id
+        ! n1: size of dim 1
+        ! n2: size of dim 2
+        ! varname: name of the variable
+        ! coef: array to fill (inout)
+        ! ngrid_first: indicates whether ngrid is the first or last dimension
+        ! on the file
 #if(defined USE_NETCDF3 || defined USE_NETCDF4)
         use netcdf
 #endif
@@ -296,6 +369,17 @@ contains
 
     subroutine read_3d_coef_from_file(n, nid, n1, n2, n3, varname, coef, &
             ngrid_first)
+        ! Parameters
+        ! ----------
+        ! n: nest index
+        ! nid: netcdf file id
+        ! n1: size of dim 1
+        ! n2: size of dim 2
+        ! n3: size of dim 3
+        ! varname: name of the variable
+        ! coef: array to fill (inout)
+        ! ngrid_first: indicates whether ngrid is the first or last dimension
+        ! on the file
 #if(defined USE_NETCDF3 || defined USE_NETCDF4)
         use netcdf
 #endif
@@ -428,31 +512,22 @@ contains
         integer             :: t
         integer             :: status
         integer             :: col,row
-        real, pointer       :: lai(:), relsm1(:), relrzsm(:)
+        real, pointer       :: lai(:), rzsm(:)
         real, pointer       :: vodval(:)
         real                :: smanom
         real                :: pred(vodoo_struc(n)%ncoef)
 
         !   map surface properties to SFC
         call getsfcvar(LIS_sfcState(n), "Leaf Area Index", lai)
-        call getsfcvar(LIS_sfcState(n), "Relative Soil Moisture Layer 1", relsm1)
-        call getsfcvar(LIS_sfcState(n), "Relative Root Zone Soil Moisture", relrzsm)
+        call getsfcvar(LIS_sfcState(n), "Root Zone Soil Moisture", rzsm)
 
         !---------------------------------------------
         ! Patch loop
         !--------------------------------------------
         do t=1, LIS_rc%npatch(n,LIS_rc%lsm_index)
 
-            smanom = relsm1(t) - max(0, min(1, relrzsm(t)))
-
-            pred(1) = lai(t) * smanom
-            pred(2) = rzsm(t) * smanom
-            pred(3) = lai(t) * rzsm(t) * smanom
-            pred(4) = smanom
-            pred(5) = lai(t)
-            pred(6) = rzsm(t)
-            pred(7) = lai(t) * rzsm(t) 
-            pred(8) = 1
+            pred(1) = lai(t)
+            pred(2) = rzsm(t)
 
             call vodoo_struc(n)%run(n, t, pred)
 
@@ -478,34 +553,106 @@ contains
         class(vodoo_type_dec), intent(inout) :: self
         integer, intent(in) :: n, t
         real, intent(in) :: pred(self%npred)
-        write(LIS_logunit,*) "[ERR] VODOO should use linear or svr model"
+        write(LIS_logunit,*) "[ERR] VODOO should use window or anom model"
         call LIS_endrun
     end subroutine VODOO_run_default
+        
 
-    subroutine VODOO_run_linear_model(self, n, t, pred)
+    subroutine VODOO_run_window_model(self, n, t, pred)
         use LIS_histDataMod
         ! !USES:
         implicit none
 
-        class(vodoo_linear_model), intent(inout) :: self
+        class(vodoo_window_model), intent(inout) :: self
         integer, intent(in) :: n, t
         real, intent(in) :: pred(self%ncoef)
 
         real                :: coefs(self%ncoef)
         logical             :: coefs_valid
+        integer             :: doy 
 
-        coefs = self%coefs(:, t)
+        doy = VODOO_current_doy()
+
+        coefs = self%coefs(:, doy, t)
 
         ! For some pixels no VOD was available and therefore no forward
         ! model was fitted. No assimilation will take place over these
         ! pixels anyways, so it's no problem to not predict anything here
         coefs_valid = (.not.isnan(coefs(1)).and.coefs(1).ne.LIS_rc%udef)
         if (coefs_valid) then
-            self%VOD(t) = sum(coefs * pred)
+            self%VOD(t) = sum(coefs * pred) + self%intercept(doy, t)
         else
             self%VOD(t)=LIS_rc%udef
         endif
-    end subroutine VODOO_run_linear_model
+    end subroutine VODOO_run_window_model
+
+
+    subroutine VODOO_run_anom_model(self, n, t, pred)
+        use LIS_histDataMod
+        ! !USES:
+        implicit none
+
+        class(vodoo_anom_model), intent(inout) :: self
+        integer, intent(in) :: n, t
+        real, intent(in) :: pred(self%ncoef)
+
+        real                :: coefs(self%ncoef)
+        real                :: actual_preds(self%ncoef)
+        real                :: vod_clim, lai_clim, rzsm_clim
+        real                :: lai, rzsm, lai_anom, rzsm_anom
+        logical             :: coefs_valid
+        integer             :: doy 
+
+        doy = VODOO_current_doy()
+
+        coefs = self%coefs(:, t)
+        vod_clim = self%vod_clim(doy, t)
+
+        lai = pred(1)
+        rzsm = pred(2)
+        lai_anom = lai - lai_clim
+        rzsm_anom = rzsm - rzsm_clim
+        
+        actual_preds(1) = lai_anom
+        actual_preds(2) = rzsm_anom
+        actual_preds(3) = lai * rzsm_anom
+        actual_preds(4) = rzsm * rzsm_anom
+        actual_preds(5) = lai * rzsm * rzsm_anom
+
+        ! For some pixels no VOD was available and therefore no forward
+        ! model was fitted. No assimilation will take place over these
+        ! pixels anyways, so it's no problem to not predict anything here
+        coefs_valid = (.not.isnan(coefs(1)).and.coefs(1).ne.LIS_rc%udef)
+        if (coefs_valid) then
+            self%VOD(t) = sum(coefs * pred) + self%intercept(doy, t) + vod_clim
+        else
+            self%VOD(t)=LIS_rc%udef
+        endif
+    end subroutine VODOO_run_anom_model
+
+
+    function VODOO_current_doy()
+        use LIS_coreMod, only : LIS_rc
+
+        integer   :: VODOO_current_doy
+        integer   :: k
+
+        data days /31,28,31,30,31,30,31,31,30,31,30,31,30/
+        data ldays /31,29,31,30,31,30,31,31,30,31,30,31,30/
+
+        VODOO_current_doy = 0
+        if((mod(yr,4).eq.0.and.mod(yr,100).ne.0) &     !correct for leap year
+             .or.(mod(yr,400).eq.0))then             !correct for y2k
+            do k=1,(LIS_rc%mo-1)
+                VODOO_current_doy=VODOO_current_doy+ldays(k)
+            enddo
+        else
+            do k=1,(LIS_rc%mo-1)
+                VODOO_current_doy=VODOO_current_doy+days(k)
+            enddo
+        endif
+        VODOO_current_doy = VODOO_current_doy + LIS_rc%da
+    end function VODOO_current_doy
 
 
     !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
