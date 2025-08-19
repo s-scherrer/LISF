c06973aafbe4be711e80646eaf5dfb8aca360bc5
Samuel Scherrer <samuel.scherrer@posteo.de>
2025-08-19 21:27:24 +0200

    CONSOLIDATION contributions

--- lis/core/LIS_histDataMod.F90
+++ lis/core/LIS_histDataMod.F90
@@ -4371,6 +4371,17 @@ contains
             n,1,ntiles,(/"m3/m3"/),1,(/"-"/),1,1,1)
     endif
 
+    call ESMF_ConfigFindLabel(modelSpecConfig,"RTM VOD:",rc=rc)
+    call get_moc_attributes(modelSpecConfig, LIS_histData(n)%head_rtm_list,&
+         "RTM_VOD",&
+         "vegetation_optical_depth",&
+         "vegetation optical depth",rc)
+    if ( rc == 1 ) then
+       call register_dataEntry(LIS_MOC_RTM_COUNT,LIS_MOC_RTM_VOD,&
+            LIS_histData(n)%head_rtm_list,&
+            n,1,ntiles,(/"-"/),1,(/"-"/),1,1,1)
+    endif
+
     call ESMF_ConfigFindLabel(modelSpecConfig,"Irrigated water:",rc=rc)
     call get_moc_attributes(modelSpecConfig, LIS_histData(n)%head_irrig_list, &
          "IrrigatedWater",&
