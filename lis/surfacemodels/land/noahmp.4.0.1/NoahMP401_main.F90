60131940e9d7d4a226f89688a9883fad2d92595b
Samuel Scherrer <samuel.scherrer@posteo.de>
2026-04-26 20:22:54 +0200

    fixes

--- lis/surfacemodels/land/noahmp.4.0.1/NoahMP401_main.F90
+++ lis/surfacemodels/land/noahmp.4.0.1/NoahMP401_main.F90
@@ -844,13 +844,13 @@ subroutine NoahMP401_main(n)
             ! maximum annual LAI to reset maxlai parameter
             if (tmp_hour.eq.0 .and. tmp_minute.eq.0) then
                 NOAHMP401_struc(n)%noahmp401(t)%maximum_lai_nsamples = NOAHMP401_struc(n)%noahmp401(t)%maximum_lai_nsamples + 1
-                NOAHMP401_struc(n)%noahmp401(t)%maximum_lai = MAX(tmp_lai, NOAHMP401_struc(n)%noahmp401(t)%maximum_lai)
-                if (tmp_month.eq.2 and tmp_day.eq.15 .and. NOAHMP401_struc(n)%noahmp401(t)%maximum_lai_nsamples > 150) then
+                NOAHMP401_struc(n)%noahmp401(t)%maximum_annual_lai = MAX(tmp_lai, NOAHMP401_struc(n)%noahmp401(t)%maximum_annual_lai)
+                if (tmp_month.eq.2 .and. tmp_day.eq.15 .and. NOAHMP401_struc(n)%noahmp401(t)%maximum_lai_nsamples > 150) then
                     ! reset maxlai mid of February to the maximum LAI value
                     ! during the year
-                    NOAHMP401_struc(n)%maxlai = NOAHMP401_struc(n)%noahmp401(t)%maximum_lai
+                    NOAHMP401_struc(n)%noahmp401(t)%maxlai = NOAHMP401_struc(n)%noahmp401(t)%maximum_annual_lai
                     NOAHMP401_struc(n)%noahmp401(t)%maximum_lai_nsamples = 0
-                    NOAHMP401_struc(n)%noahmp401(t)%maximum_lai = 0.0
+                    NOAHMP401_struc(n)%noahmp401(t)%maximum_annual_lai = 0.0
                 end if
             end if
 
